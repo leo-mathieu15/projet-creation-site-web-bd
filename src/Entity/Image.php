0c213b6cad033ea2101e5e8002dc553fdbb58a54
Romain MONNEY <romain.monney@etudiant.univ-reims.fr>
2023-06-12 15:35:11 +0200

    add définition and getters Image.php

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -1,1 +1,26 @@
 <?php
+declare(strict_types=1);
+
+namespace Entity;
+
+class Movie
+{
+    private int $id;
+    private string $jpeg;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+}
