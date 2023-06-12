898dbd9de511cf876f01d871202ea2371a6ff037
Romain MONNEY <romain.monney@etudiant.univ-reims.fr>
2023-06-12 15:31:08 +0200

    ajout définition Movie.php

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -1,1 +1,19 @@
 <?php
+
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Movie
+{
+    private int $id;
+    private int $posterId;
+    private string $originalLanguage;
+    private string $originalTitle;
+    private string $overview;
+    private string $releaseDate;
+    private int $runtime;
+    private string $tagline;
+    private string $title;
+}
