9e6220bda94927d7021cfebf2573fd3e980e9592
math0139 <leo.mathieu@etudiant.univ-reims.fr>
2023-06-13 22:13:53 +0200

    Ajout movie.php

---
+++ public/movie.php
@@ -0,0 +1,31 @@
+<?php
+
+use Entity\Movie;
+use Html\WebPage;
+use Database\MyPdo;
+
+$webPage = new WebPage();
+
+MyPDO::setConfiguration('mysql:host=mysql;dbname=monn0042_movie;charset=utf8', 'monn0042', 'monn0042');
+
+$movie = Movie::findById((int)$_GET['Id']);
+
+$webPage->appendToHead("<link rel='stylesheet' type='text/css' href='css/style.css' />");
+$webPage->appendContent("<header>Film - {$movie->getTitle()}</header>");
+$webPage->appendContent("<div class='details'>");
+
+$webPage->appendContent(<<<HTML
+<img class="movie__poster" src="image.php?Id={$movie->getPosterId()}" alt="poster de {$movie->getTitle()}"></a><br/>
+HTML);
+$webPage->appendContent(
+    "{$movie->getTitle()}<br/>".
+    "{$movie->getReleaseDate()}<br/>".
+    "{$movie->getOriginalTitle()}<br/>".
+    "{$movie->getTagLine()}<br/>".
+    "{$movie->getOverview()}<br/>"
+);
+
+$webPage->appendContent("</div>"."<footer>Dernière modification : {$webPage->getLastModification()}</footer>");
+
+$page = $webPage->toHTML();
+echo $page;
