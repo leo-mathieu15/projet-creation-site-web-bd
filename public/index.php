011fa98e0ba94345589ea5d415d0c171226dbc40
Romain MONNEY <romain.monney@etudiant.univ-reims.fr>
2023-06-13 21:37:07 +0200

    ajout css fichier séparer et link index style.css

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@ require_once '../vendor/autoload.php';
 
 use Database\MyPdo;
 use Html\WebPage;
-use Entity\Image;
 
 $webPage = new WebPage("Films");
 
@@ -15,6 +14,7 @@ $movies = (new Entity\Collection\MovieCollection())->findAll();
 
 $webPage->appendContent("<header>Films</header>");
 $webPage->appendContent("<div class='films'>");
+$webPage->appendToHead("<link rel='stylesheet' type='text/css' href='css/style.css' />");
 
 foreach ($movies as $movie) {
     $webPage->appendContent("<a id='box' href='movie.php?Id={$movie->getId()}'>".$webPage->escapeString($movie->getTitle()));
@@ -25,62 +25,7 @@ HTML);
 
 $webPage->appendContent("</div>");
 
-$webPage->appendCss("     
-#box {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1%;
-    margin: 1%;
-    width: 9em;
-    height: 9em;
-    border-style: solid;
-    border-color:#FFFFFF;
-    background: #00AAFF;
-    text-decoration: none;
-    color: black;
-}
 
-.films {
-    display: flex;
-    justify-content: center;
-    flex-direction: row;
-    flex-wrap: wrap;
-    margin-top:auto;
-    width:auto;
-    height:94vh;
-    overflow: auto;
-    flex-grow:1;
-}
-
-html, body{
-    height:100%;
-    margin:0;
-    background-color:black;
-}
-
-@media screen and (max-height:640px){
-    .films {
-        height:85vh;
-    }
-}
-
-header{
-    background-color:#000088;
-    color:#FFFFFF;
-    text-align:center;
-    border-style:double;
-    border-color:#FFFFFF;
-}
-
-footer{
-    background-color:#000066;
-    color:#FFFFFF;
-    text-align:center;
-    border-style:double;
-    border-color:#FFFFFF;
-}
-");
 
 $webPage->appendContent("<footer>Dernière modification : {$webPage->getLastModification()}</footer>");
 
