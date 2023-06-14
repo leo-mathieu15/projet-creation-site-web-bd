420989b31e6f9a7c9c6f1a5f0be31862e824aa3a
Romain MONNEY <romain.monney@etudiant.univ-reims.fr>
2023-06-14 16:55:59 +0200

    ajout movies in actor.php

--- public/actor.php
+++ public/actor.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 use Entity\Actor;
 use Html\WebPage;
-use Database\MyPdo;
+use Entity\Collection\MovieCollection;
+use Entity\Cast;
 
 $webPage = new WebPage();
 
 
 $actor = Actor::findById((int)$_GET['Id']);
+$movies = MovieCollection::findByIdpeople((int)$_GET['Id']);
 
 $webPage->appendToHead("<link rel='stylesheet' type='text/css' href='css/style.css' />");
 $webPage->appendContent("<header>Film - {$actor->getName()}</header>");
@@ -27,7 +29,16 @@ $webPage->appendContent(<<<HTML
 <div>{$actor->getBiography()}</div><br>
 </div>
 HTML);
-
+foreach ($movies as $movie) {
+    $cast = Cast::findById($movie->getId(),$actor->getId());
+    $webPage->appendContent(<<<HTML
+<div class="acteurFilm">
+<img class="movie__poster" src="image.php?Id={$movie->getPosterId()}" alt="poster de {$movie->getTitle()}"></a>
+<a>{$movie->getTitle()}{$movie->getReleaseDate()}</a>
+<a>{$cast->getRole()}</a>
+</div>
+HTML);
+}
 $webPage->appendContent("</div>"."<footer>Dernière modification : {$webPage->getLastModification()}</footer>");
 
 $page = $webPage->toHTML();
