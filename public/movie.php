bb69958d57c5971fb6e1604a0e33f9285c60c806
Romain MONNEY <romain.monney@etudiant.univ-reims.fr>
2023-06-14 10:57:08 +0200

    changement style dans movie.php

--- public/movie.php
+++ public/movie.php
@@ -11,18 +11,17 @@ $movie = Movie::findById((int)$_GET['Id']);
 
 $webPage->appendToHead("<link rel='stylesheet' type='text/css' href='css/style.css' />");
 $webPage->appendContent("<header>Film - {$movie->getTitle()}</header>");
-$webPage->appendContent("<div class='details'>");
+$webPage->appendContent("<div class='movie'>");
 
 $webPage->appendContent(<<<HTML
 <img class="movie__poster" src="image.php?Id={$movie->getPosterId()}" alt="poster de {$movie->getTitle()}"></a><br/>
+<div class="details">
+<a class="basicinfo">{$movie->getTitle()}</a>
+<a class="basicinfo">{$movie->getReleaseDate()}</a><br/>
+<a>{$movie->getOriginalTitle()}</a><br/>
+<a>{$movie->getTagLine()}</a><br/>
+<a>{$movie->getOverview()}</a><br/></div>
 HTML);
-$webPage->appendContent(
-    "{$movie->getTitle()}<br/>".
-    "{$movie->getReleaseDate()}<br/>".
-    "{$movie->getOriginalTitle()}<br/>".
-    "{$movie->getTagLine()}<br/>".
-    "{$movie->getOverview()}<br/>"
-);
 
 $webPage->appendContent("</div>"."<footer>Dernière modification : {$webPage->getLastModification()}</footer>");
 
